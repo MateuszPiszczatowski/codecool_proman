91dad55c1525133b24bc0ccf0cd36101e8490a00
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2026-07-12 17:30:58 +0200

    Fix shared status data loss and refactor handler

--- data_handler/status_handler.py
+++ data_handler/status_handler.py
@@ -3,6 +3,9 @@
     Queries regarding statuses.
 """
 from typing import Any
+
+from psycopg2.extras import RealDictRow
+
 from . import connection_manager
 
 DEFAULT_STATUSES = ['new', 'in progress', 'testing', 'done']
@@ -21,60 +24,53 @@ def add_default_statuses(board_id: int) -> None:
         post_status(board_id, status)
 
 
-def get_status(status_id: int) -> Any:
+def get_status(status_id: int) -> RealDictRow | None:
     query: str = """
-    SELECT title
-    FROM statuses
-    WHERE id = %(id)s
+        SELECT title
+        FROM statuses
+        WHERE id = %(id)s
     """
-    matching_status: Any = connection_manager.execute_select(query, {"id": status_id})
+    matching_status: Any = connection_manager.execute_select(query, {"id": status_id}, fetchall=False)
 
     return matching_status
 
 
-def get_board_statuses(board_id: int) -> Any:
+def get_board_statuses(board_id: int) -> list[RealDictRow]:
     query: str = """
-    SELECT *
-    FROM board_statuses AS bs
-    LEFT JOIN statuses AS s ON s.id = bs.status_id
-    WHERE bs.board_id = %(id)s
+        SELECT *
+        FROM board_statuses AS bs
+        LEFT JOIN statuses AS s ON s.id = bs.status_id
+        WHERE bs.board_id = %(id)s
     """
     matching_statuses: Any = connection_manager.execute_select(query, {"id": board_id})
 
     return matching_statuses
 
 
-def post_status(board_id: int, title: str) -> Any:
-    query_select_status: str = """
-    SELECT id FROM statuses
-    WHERE title = %s
-    """
+def post_status(board_id: int, title: str) -> RealDictRow | None:
     query_statuses: str = """
-    INSERT INTO statuses (title)
-    VALUES(
-        %(title)s
-    )
-    RETURNING id
+        INSERT INTO statuses (title)
+        VALUES (%(title)s)
+        RETURNING id
     """
     query_status_order: str = """
-    SELECT
-            (MAX(status_order) + 1) AS status_order
-            FROM board_statuses
-            WHERE board_id = %(board_id)s
-            """
-    query_board_statuses: str = """
-    INSERT INTO board_statuses (status_id, board_id, status_order)
-    VALUES (
-        %(status_id)s, %(board_id)s, %(status_order)s
-        
-    )
+        SELECT (MAX(status_order) + 1) AS status_order 
+        FROM board_statuses
+        WHERE board_id = %(board_id)s
     """
-    status = connection_manager.execute_select(query_select_status, [title], False)
-    status: Any = status if status is not None else connection_manager.execute_dml(query_statuses, {"title": title}, 'one')
-    status_order = connection_manager.execute_select(query_status_order, variables={'board_id': board_id}, fetchall=False)
-    connection_manager.execute_dml(query_board_statuses,
-                                   {"status_id": status["id"], "board_id": board_id,
-                              "status_order": status_order['status_order'] if status_order['status_order'] is not None else 1})
+    query_board_statuses: str = """
+        INSERT INTO board_statuses (status_id, board_id, status_order)
+        VALUES (%(status_id)s, %(board_id)s, %(status_order)s)
+    """
+    status = connection_manager.execute_dml(query_statuses, {"title": title}, 'one')
+    if not status or isinstance(status,list):
+        return None
+    status_order = 1
+    status_order_result = connection_manager.execute_select(query_status_order, variables={'board_id': board_id}, fetchall=False)
+    if status_order_result and status_order_result.get('status_order'):
+        status_order = status_order_result['status_order']
+    connection_manager.execute_dml(query_board_statuses, {"status_id": status["id"], "board_id": board_id,
+                                                          "status_order": status_order})
     return status
 
 
@@ -83,9 +79,9 @@ def patch_status(status_id: int, data: dict[str, Any]) -> None:
         UPDATE statuses
         SET title = %(title)s
         WHERE id = %(id)s
-        """
-    data.update({"id": status_id})
-    connection_manager.execute_dml(query, data)
+    """
+    variables = {**data, "id": status_id}
+    connection_manager.execute_dml(query, variables)
 
 
 def patch_status_order(status_id: int, data: dict[str, Any]) -> None:
@@ -96,23 +92,23 @@ def patch_status_order(status_id: int, data: dict[str, Any]) -> None:
         SET status_order = %(status_order)s
         WHERE status_id = %(id)s
     """
-    data.update({"id": status_id})
-    connection_manager.execute_dml(query, data)
+    variables = {**data, "id": status_id}
+    connection_manager.execute_dml(query, variables)
 
 
 def delete_status(board_id: int, status_id: int) -> None:
     query_statuses: str = """
-    DELETE FROM statuses
-    WHERE id = %(status_id)s
+        DELETE FROM statuses
+        WHERE id = %(status_id)s
     """
     query_board_statuses: str = """
-    DELETE FROM board_statuses
-    WHERE status_id = %(status_id)s
-    AND board_id = %(board_id)s
+        DELETE FROM board_statuses
+        WHERE status_id = %(status_id)s
+        AND board_id = %(board_id)s
     """
     query_cards: str = """
-    DELETE FROM cards
-    WHERE status_id = %(status_id)s
+        DELETE FROM cards
+        WHERE status_id = %(status_id)s
     """
     connection_manager.execute_dml(query_cards, {"status_id": status_id})
     connection_manager.execute_dml(query_board_statuses,
